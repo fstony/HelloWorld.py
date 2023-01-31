354fcd7a3c2d91ab861cbb1d7f70da1f20df0e6e
fstony <fs.tony@outlook.com>
2023-01-31 09:12:28 -0600

    Tareas

--- tareas/tarea_2/triangulo.py
+++ tareas/tarea_2/triangulo.py
@@ -1,9 +1,7 @@
 #un programa que realice un triangulo de numeros segun el numero ingresado.
-num1 = int(input("Digite un numero entero:"))
-if num1 < 1:
-    print("Error: numero debe de ser mayor a 0")    
-else:
-    concatenado = ""
-    for i in range (1,num1 +1):
-     concatenado + str(i)+1 + ""
-    print(concatenado)
+n = int(input("Numero:"))
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j , end=" ")
+    print("")
